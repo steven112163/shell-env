99d4b5022bdc6b1c6979bc3547cf81a9e9ed5b5b
steven112163 <steven112163@gmail.com>
2022-10-16 19:43:37 +0800

    Add statusbar settings

---
+++ .settings.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Settings
+
+source $HOME/.aliases.sh
+source $HOME/.statusbar.sh
+
+export GREP_COLOR="1;33"
+export HISTCONTROL=ignoreboth
+export TERM=xterm-256color
+export LANG=en_US.UTF-8
+export LC_ALL=${LANG}
+export LC_CTYPE=en_US.UTF-8
